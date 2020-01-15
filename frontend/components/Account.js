0414ef375d079fc5f52d58cf370613ebedb09016
Wes Bos <wesbos@gmail.com>
2020-01-15 15:26:29 -0500

    User can now edit their name, closes #5

--- frontend/components/Account.js
+++ frontend/components/Account.js
@@ -1,19 +1,59 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import gql from 'graphql-tag';
+import { useMutation } from '@apollo/react-hooks';
 import { useUser } from './User';
+import useForm from '../lib/useForm';
 import Form from './styles/Form';
-import SignIn from './Signin';
+import PleaseSignIn from './PleaseSignIn';
+import FormItem from './FormItem';
 
-function PleaseSignIn({ children }) {
-  const me = useUser();
-  if(!me)
-
-}
+const UPDATE_USER_MUTATION = gql`
+  mutation UPDATE_USER_MUTATION($name: String!, $id: ID!) {
+    updateUser(id: $id, data: { name: $name }) {
+      id
+      name
+    }
+  }
+`;
 
 function Account() {
   const me = useUser();
-  if (!me) return <SignIn />;
-  return <p>Hello {me.name}</p>;
+  const { inputs, handleChange } = useForm({
+    name: me.name,
+  });
+  console.log(me);
+  const [updateUser, { data, error, loading }] = useMutation(
+    UPDATE_USER_MUTATION,
+    {
+      variables: {
+        id: me.id,
+        name: inputs.name,
+      },
+    }
+  );
+  return (
+    <Form
+      onSubmit={async e => {
+        e.preventDefault();
+        await updateUser();
+        console.log(data);
+      }}
+    >
+      <fieldset disabled={loading}>
+        <FormItem inputs={inputs} onChange={handleChange} name="name" />
+        <button type="submit">Updat{loading ? 'ing' : 'e'}</button>
+      </fieldset>
+    </Form>
+  );
 }
 
-export default Account;
+function Component() {
+  return (
+    <PleaseSignIn>
+      <Account></Account>
+    </PleaseSignIn>
+  );
+}
+
+export default Component;
--- frontend/components/PleaseSignIn.js
+++ frontend/components/PleaseSignIn.js
@@ -1,22 +1,11 @@
-import { Query } from 'react-apollo';
-import { Children } from 'react';
 import { useUser } from './User';
 import Signin from './Signin';
 
 function PleaseSignIn({ children }) {
   const me = useUser();
-  // TODO loading???
-  const loading = false;
-  if (loading) return <p>Loading...</p>;
-  if (!me) {
-    return (
-      <div>
-        <p>Please Sign In before Continuing</p>
-        <Signin />
-      </div>
-    );
-  }
-  return children();
+  // TODO Loading..
+  if (!me) return <Signin />;
+  return children;
 }
 
 export default PleaseSignIn;
