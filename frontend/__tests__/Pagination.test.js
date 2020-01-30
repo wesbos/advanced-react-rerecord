dcfac3c3093a8b137b50ebf87feac9c94eeff05e
Wes Bos <wesbos@gmail.com>
2020-01-30 17:13:28 -0500

    Pagination tests

--- frontend/__tests__/Pagination.test.js
+++ frontend/__tests__/Pagination.test.js
@@ -1,15 +1,6 @@
-// import { mount } from 'enzyme';
-import { render, fireEvent, screen } from '@testing-library/react';
-import wait from 'waait';
-// import toJSON from 'enzyme-to-json';
-import Router from 'next/router';
+import { render } from '@testing-library/react';
 import { MockedProvider } from '@apollo/react-testing';
 import Pagination, { PAGINATION_QUERY } from '../components/Pagination';
-
-Router.router = {
-  push() {},
-  prefetch() {},
-};
 
 function makeMocksFor(length) {
   return [
