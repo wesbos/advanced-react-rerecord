cc4edea312288ac07661759e99c210b3ee9323c2
Wes Bos <wesbos@gmail.com>
2020-02-25 14:52:20 -0500

    endpoints

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:3000/admin/api`;
-export const prodEndpoint = `http://localhost:3000/admin/api`;
+export const prodEndpoint = `http://68.183.201.32:3000/admin/api`;
 export const perPage = 4;
