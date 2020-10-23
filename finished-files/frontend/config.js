3ccf263b262a28149e100bb7451637376c20e769
Wes Bos <wesbos@gmail.com>
2020-10-23 16:00:55 -0400

    endpoint

--- finished-files/frontend/config.js
+++ finished-files/frontend/config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:3000/admin/api`;
-export const prodEndpoint = `http://68.183.201.32:3000/admin/api`;
+export const prodEndpoint = `https://sick-fits-2ybvv.ondigitalocean.app/admin/api`;
 export const perPage = 4;
